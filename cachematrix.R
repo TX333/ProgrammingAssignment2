a3d5a9990ab4866996352e9bd2a0e0e38be18b4e
TX333 <tx333@me.com>
2015-08-17 18:36:11 -0500

    ProgrammingAssignment2
    Updated 8/17/15 1836hrs CST

--- cachematrix.R
+++ cachematrix.R
@@ -21,7 +21,7 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
-## Testing for updates in the code
+
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         i <- x$getinverse()
